05a37503b9b2a775f07a68b6271fafa53f886c00
hynek2001 <hynek.los@gmail.com>
2020-08-01 14:00:07 +0200

    README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+#### Finance library
+    * mfin.yahoo
+        getNasdaqStock
+        
+            ```python
+            from mfin.yahoo import getNasdaqStock
+            getNasdaqStock("AMZN")
+             
+            ```
+        
+        getNasdaqStocks
---
+++ mfin/yahoo.py
@@ -0,0 +1,51 @@
+"""
+library for fetching live data from yahoo
+
+
+"""
+
+import requests
+from bs4 import BeautifulSoup, NavigableString
+import locale
+import re
+
+
+# locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
+# konverze do floatu... locale.atof('1,000,000.53')
+
+
+def getNasdaqStocks(stocks: list) -> dict:
+    """get list of nasdaq stock"""
+    rsDa = {}
+    for ii in stocks:
+        rsDa[ii] = getNasdaqStock(stock=ii)
+    return rsDa
+
+
+def getNasdaqStock(stock: str) -> dict:
+    """get nasdaq stock"""
+    r = requests.get(f"https://finance.yahoo.com/quote/AMZN?p={stock}&.tsrc=fin-srch").text
+    soup = BeautifulSoup(r, 'html.parser')
+    alldata = soup.find_all('tbody')
+    ydata = {}
+
+    def getLast(tag):
+        for ii in tag.children:
+            if isinstance(ii, NavigableString):
+                return ii
+            else:
+                return getLast(ii)
+
+    for tables in alldata:
+        for items in tables.find_all('tr'):
+            name = items.find_all("td")[0]
+            value = getLast(items.find_all("td")[1])
+            valRes = re.match("^[\d\,]+\.*\d+$", value)
+            if valRes != None:
+                locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+                ffR = locale.atof(value)
+                ydata[name.text] = ffR
+            else:
+                ydata[name.text] = value
+
+    return ydata
